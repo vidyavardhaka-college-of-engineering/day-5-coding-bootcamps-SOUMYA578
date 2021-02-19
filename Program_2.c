076ecf3e044dc23c90fc1924fb612a6dc791bcdb
SOUMYA578 <20cse0092@vvce.ac.in>
2021-02-19 11:59:25 +0000

    Gourav R Soumya V

--- Program_2.c
+++ Program_2.c
@@ -3,12 +3,23 @@
 
 #include<stdio.h>
 
+
 int main()
 {
 	char str[10];
+  char rev[10];
+  int count=0,begin,end;
 	scanf("%s", str);
-
-	//Write your code here
+while(str[count] !='\0' )
+count++;
+end=count-1;
+for(begin=0;begin<count;begin++)
+{
+  rev[begin]=str[end];
+  end--;
+}
+   rev[begin]=='\0';
+   printf("%s\n",rev);
 
 	return 0;
 }
