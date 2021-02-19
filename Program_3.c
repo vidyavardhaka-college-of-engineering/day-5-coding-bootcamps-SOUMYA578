c883a86ff7efa18891eb7f81a01d75f13a9cb69f
SOUMYA578 <20cse0092@vvce.ac.in>
2021-02-19 12:21:08 +0000

    Soumya V Gourav R

--- Program_3.c
+++ Program_3.c
@@ -6,13 +6,27 @@
 //output:
 //Palindrome
 #include<stdio.h>
+#include<string.h>
 
 int main()
 {
 	char str[10];
+  int len, flag=1,i;
 	scanf("%s", str);
 
+len = strlen(str);
+int lastIndex = len-1;
+for(i=0;i<len/2;i++)
+{
+  if(str[i] != str[lastIndex-i])
+  {flag =0;
+  break;
+  }
+}
 	//Write your code here
-	
+	if (flag == 1)
+     printf("Palindrome\n");
+  else
+     printf("it's not\n");
 	return 0;
 }
